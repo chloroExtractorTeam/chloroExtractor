d7a53a51c082eaf21b8c39e4dd6976a265e0c319
Markus Ankenbrand <markus.ankenbrand@stud-mail.uni-wuerzburg.de>
2013-06-21 10:01:10 +0200

    iteration moved to R script

---
+++ find_chloro_kmer_peak/find_chloro_kmer_peak_lowess.R
@@ -0,0 +1,10 @@
+findMaxLowess <- function(data){
+	xMax = 0
+	upperBound = max(data[,1])
+	while(xMax < 1 & upperBound > 100){
+		maxLowess<-lowess(data[,1][data[,1]<upperBound], data[,2][data[,1]<upperBound]);
+		xMax<-which.max(maxLowess[[2]])[1];
+		upperBound = 0.9*upperBound;
+	}
+	return(maxLowess)
+}
