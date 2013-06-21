7884ae59e8c34fc19cde3b9d2a4d6c325196af4c
Markus Ankenbrand <markus.ankenbrand@stud-mail.uni-wuerzburg.de>
2013-06-21 10:09:47 +0200

    find chloro kmer peak lowess new

--- find_chloro_kmer_peak/find_chloro_kmer_peak_lowess.R
+++ find_chloro_kmer_peak/find_chloro_kmer_peak_lowess.R
@@ -2,9 +2,9 @@ findMaxLowess <- function(data){
 	xMax = 0
 	upperBound = max(data[,1])
 	while(xMax < 1 & upperBound > 100){
-		maxLowess <- lowess(data[,1][data[,1]<upperBound], data[,2][data[,1]<upperBound]);
-		xMax <- which.max(maxLowess[[2]])[1];
-		upperBound <- 0.9*upperBound;
+		maxLowess<-lowess(data[,1][data[,1]<upperBound], data[,2][data[,1]<upperBound]);
+		xMax<-which.max(maxLowess[[2]])[1];
+		upperBound = 0.9*upperBound;
 	}
 	return(maxLowess)
 }
