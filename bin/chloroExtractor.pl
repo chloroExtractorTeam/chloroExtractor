9491369a913265a724a94f9d7dc99ed786141c1a
Markus Ankenbrand <markus.ankenbrand@stud-mail.uni-wuerzburg.de>
2013-06-24 12:42:57 +0200

    steps skippable

--- bin/chloroExtractor.pl
+++ bin/chloroExtractor.pl
@@ -71,6 +71,22 @@ prefix for the output files. Default is current directory and a prefix
 
 $options{'prefix=s'} = \(my $opt_prefix);
 
+=item [--skip=<STRING>] 
+
+Comma separated list of steps to skip:
+1 Kmer counting, merging and histogramming
+2 Find Chloro Peak
+3 Dump Kmers
+4 Filter Raw Reads
+5 Dump Reads
+6 Error correct Dumped Reads
+7 Assemble Reads
+8 Filter contigs
+
+=cut
+
+$options{'skip=s'} = \(my $opt_skip="");
+
 =item [--jellyfish-kmer-size=<INT>] 
 
 desired kmer size for jellyfish
@@ -189,115 +205,157 @@ pod2usage(-msg => 'ErrorCorrectReads.pl not in $PATH and binary (--allpath-corre
 pod2usage(-msg => 'sickle not in $PATH and binary (--sickle-bin) not specified', -verbose => 0) unless ($opt_sickle_bin);
 pod2usage(-msg => 'velvet not in $PATH and binary (--velvet-bin) not specified', -verbose => 0) unless ($opt_velvet_bin);
 
+my %skip = ();
+$skip{$_} = 1 foreach split(/,/,$opt_skip);
+
 $opt_prefix = get_prefix() unless $opt_prefix;
 my ($prefix_name,$prefix_dir) = fileparse($opt_prefix);
 
-$vwga->verbose('Counting kmers');
-$vwga->hline();
-my $jellyfish_count_cmd = jellyfish_count_command();
-$vbash->verbose( $jellyfish_count_cmd );
-my $jellyfish_count_re = qx($jellyfish_count_cmd); 
-$vwga->nline();
-$vplain->verbose($jellyfish_count_re) if $jellyfish_count_re;
-$vwga->exit('ERROR: Counting kmers failed') if $?>> 8;
+if(exists $skip{1}){
+	$vwga->verbose('Skipping kmer counting, merging und histogramming');
+}
+else{
+	$vwga->verbose('Counting kmers');
+	$vwga->hline();
+	my $jellyfish_count_cmd = jellyfish_count_command();
+	$vbash->verbose( $jellyfish_count_cmd );
+	my $jellyfish_count_re = qx($jellyfish_count_cmd); 
+	$vwga->nline();
+	$vplain->verbose($jellyfish_count_re) if $jellyfish_count_re;
+	$vwga->exit('ERROR: Counting kmers failed') if $?>> 8;
+	
+	$vwga->verbose('Merging kmer counts');
+	$vwga->hline();
+	my $jellyfish_merge_cmd = jellyfish_merge_command();
+	$vbash->verbose( $jellyfish_merge_cmd );
+	my $jellyfish_merge_re = qx($jellyfish_merge_cmd); 
+	$vwga->nline();
+	$vplain->verbose($jellyfish_merge_re) if $jellyfish_merge_re;
+	$vwga->exit('ERROR: Merging kmer counts') if $?>> 8;
+	
+	$vwga->verbose('Histogramming kmer counts');
+	$vwga->hline();
+	my $jellyfish_histo_cmd = jellyfish_histo_command();
+	$vbash->verbose( $jellyfish_histo_cmd );
+	my $jellyfish_histo_re = qx($jellyfish_histo_cmd); 
+	$vwga->nline();
+	$vplain->verbose($jellyfish_histo_re) if $jellyfish_histo_re;
+	$vwga->exit('ERROR: Histogramming kmer counts failed') if $?>> 8;
+}
 
-$vwga->verbose('Merging kmer counts');
-$vwga->hline();
-my $jellyfish_merge_cmd = jellyfish_merge_command();
-$vbash->verbose( $jellyfish_merge_cmd );
-my $jellyfish_merge_re = qx($jellyfish_merge_cmd); 
-$vwga->nline();
-$vplain->verbose($jellyfish_merge_re) if $jellyfish_merge_re;
-$vwga->exit('ERROR: Merging kmer counts') if $?>> 8;
+if(exists $skip{2}){
+	$vwga->verbose('Skipping peak detection');
+}
+else{
+	$vwga->verbose('Finding chloroplast peak in kmer histogram');
+	$vwga->hline();
+	my $findChloroPeak_cmd = findChloroPeak_command();
+	$vbash->verbose( $findChloroPeak_cmd );
+	my $findChloroPeak_re = qx($findChloroPeak_cmd); 
+	$vwga->nline();
+	$vplain->verbose($findChloroPeak_re) if $findChloroPeak_re;
+	$vwga->exit('ERROR: Chloroplast peak detection failed') if $?>> 8;
+}
 
-$vwga->verbose('Histogramming kmer counts');
-$vwga->hline();
-my $jellyfish_histo_cmd = jellyfish_histo_command();
-$vbash->verbose( $jellyfish_histo_cmd );
-my $jellyfish_histo_re = qx($jellyfish_histo_cmd); 
-$vwga->nline();
-$vplain->verbose($jellyfish_histo_re) if $jellyfish_histo_re;
-$vwga->exit('ERROR: Histogramming kmer counts failed') if $?>> 8;
+if(exists $skip{3}){
+	$vwga->verbose('Skipping kmer dump');
+}
+else{
+	open(IN, "<$opt_prefix"."_minmax.tsv") or die "Can't open file $opt_prefix"."_minmax.tsv$!";
+	my ($min, $max) = split(/\t/,<IN>);
+	chomp $max;
+	$max *= 3; # Take three times the maximal x value (expect IR at double)
+	$vwga->verbose('Dumping kmers in count range $min - $max');
+	$vwga->hline();
+	my $jellyfish_dump_cmd = jellyfish_dump_command();
+	$vbash->verbose( $jellyfish_dump_cmd );
+	my $jellyfish_dump_re = qx($jellyfish_dump_cmd); 
+	$vwga->nline();
+	$vplain->verbose($jellyfish_dump_re) if $jellyfish_dump_re;
+	$vwga->exit('ERROR: Dumping kmers failed') if $?>> 8;
+}
 
-$vwga->verbose('Finding chloroplast peak in kmer histogram');
-$vwga->hline();
-my $findChloroPeak_cmd = findChloroPeak_command();
-$vbash->verbose( $findChloroPeak_cmd );
-my $findChloroPeak_re = qx($findChloroPeak_cmd); 
-$vwga->nline();
-$vplain->verbose($findChloroPeak_re) if $findChloroPeak_re;
-open(IN, "<$opt_prefix"."_minmax.tsv") or die "Can't open file $opt_prefix"."_minmax.tsv$!";
-my ($min, $max) = split(/\t/,<IN>);
-chomp $max;
-$vwga->exit('ERROR: Chloroplast peak detection failed') if $?>> 8;
+if(exists $skip{4}){
+	$vwga->verbose('Skipping quality trimming');
+}
+else{
+	$vwga->verbose('Quality trimming raw reads');
+	$vwga->hline();
+	my $quality_trimming_cmd = quality_trimming_command();
+	$vbash->verbose( $quality_trimming_cmd );
+	my $quality_trimming_re = qx($quality_trimming_cmd); 
+	$vwga->nline();
+	$vplain->verbose($quality_trimming_re) if $quality_trimming_re;
+	$vwga->exit('ERROR: Quality trimming failed') if $?>> 8;
+}
 
-$max *= 3; # Take three times the maximal x value (expect IR at double)
+if(exists $skip{5}){
+	$vwga->verbose('Skipping read dump');
+}
+else{
+	$vwga->verbose('Dumping reads by kmer coverage');
+	$vwga->hline();
+	my $initial_read_dump_cmd = initial_read_dump_command();
+	$vbash->verbose( $initial_read_dump_cmd );
+	my $initial_read_dump_re = qx($initial_read_dump_cmd); 
+	$vwga->nline();
+	$vplain->verbose($initial_read_dump_re) if $initial_read_dump_re;
+	$vwga->exit('ERROR: Dumping reads by kmer coverage failed') if $?>> 8;
+}
 
-$vwga->verbose('Dumping kmers in count range $min - $max');
-$vwga->hline();
-my $jellyfish_dump_cmd = jellyfish_dump_command();
-$vbash->verbose( $jellyfish_dump_cmd );
-my $jellyfish_dump_re = qx($jellyfish_dump_cmd); 
-$vwga->nline();
-$vplain->verbose($jellyfish_dump_re) if $jellyfish_dump_re;
-$vwga->exit('ERROR: Dumping kmers failed') if $?>> 8;
-
-$vwga->verbose('Quality trimming raw reads');
-$vwga->hline();
-my $quality_trimming_cmd = quality_trimming_command();
-$vbash->verbose( $quality_trimming_cmd );
-my $quality_trimming_re = qx($quality_trimming_cmd); 
-$vwga->nline();
-$vplain->verbose($quality_trimming_re) if $quality_trimming_re;
-$vwga->exit('ERROR: Quality trimming failed') if $?>> 8;
-
-$vwga->verbose('Dumping reads by kmer coverage');
-$vwga->hline();
-my $initial_read_dump_cmd = initial_read_dump_command();
-$vbash->verbose( $initial_read_dump_cmd );
-my $initial_read_dump_re = qx($initial_read_dump_cmd); 
-$vwga->nline();
-$vplain->verbose($initial_read_dump_re) if $initial_read_dump_re;
-$vwga->exit('ERROR: Dumping reads by kmer coverage failed') if $?>> 8;
-
-$vwga->verbose('Error correcting reads');
-$vwga->hline();
-my $error_correction_cmd = error_correction_command();
-$vbash->verbose( $error_correction_cmd );
-my $error_correction_re = qx($error_correction_cmd); 
-$vwga->nline();
-$vplain->verbose($error_correction_re) if $error_correction_re;
-$vwga->exit('ERROR: Error correcting reads failed') if $?>> 8;
+if(exists $skip{6}){
+	$vwga->verbose('Skipping error correction');
+}
+else{
+	$vwga->verbose('Error correcting reads');
+	$vwga->hline();
+	my $error_correction_cmd = error_correction_command();
+	$vbash->verbose( $error_correction_cmd );
+	my $error_correction_re = qx($error_correction_cmd); 
+	$vwga->nline();
+	$vplain->verbose($error_correction_re) if $error_correction_re;
+	$vwga->exit('ERROR: Error correcting reads failed') if $?>> 8;
+}
 
 ########### velvet assembly
-$vwga->verbose('Assembly of the corrected reads');
-$vwga->hline();
-$vwga->verbose('Preparing assembly: velveth');
-$vwga->hline();
-my $velveth_cmd = velveth_command();
-$vbash->verbose( $velveth_cmd );
-my $velveth_re = qx($velveth_cmd); 
-$vwga->nline();
-$vplain->verbose($velveth_re) if $velveth_re;
-$vwga->exit('ERROR: velveth failed') if $?>> 8;
-$vwga->verbose('Executing assembly: velvetg');
-$vwga->hline();
-my $velvetg_cmd = velvetg_command();
-$vbash->verbose( $velvetg_cmd );
-my $velvetg_re = qx($velvetg_cmd); 
-$vwga->nline();
-$vplain->verbose($velvetg_re) if $velvetg_re;
-$vwga->exit('ERROR: velvetg failed') if $?>> 8;
+if(exists $skip{7}){
+	$vwga->verbose('Skipping assembly');
+}
+else{
+	$vwga->verbose('Assembly of the corrected reads');
+	$vwga->hline();
+	$vwga->verbose('Preparing assembly: velveth');
+	$vwga->hline();
+	my $velveth_cmd = velveth_command();
+	$vbash->verbose( $velveth_cmd );
+	my $velveth_re = qx($velveth_cmd); 
+	$vwga->nline();
+	$vplain->verbose($velveth_re) if $velveth_re;
+	$vwga->exit('ERROR: velveth failed') if $?>> 8;
+	$vwga->verbose('Executing assembly: velvetg');
+	$vwga->hline();
+	my $velvetg_cmd = velvetg_command();
+	$vbash->verbose( $velvetg_cmd );
+	my $velvetg_re = qx($velvetg_cmd); 
+	$vwga->nline();
+	$vplain->verbose($velvetg_re) if $velvetg_re;
+	$vwga->exit('ERROR: velvetg failed') if $?>> 8;
+}
 
 ########### contig Filtering (simple size filter)
-$vwga->verbose('Filtering contigs by size');
-$vwga->hline();
-my $sizefilter_contigs_cmd = sizefilter_contigs_command();
-$vbash->verbose( $sizefilter_contigs_cmd );
-my $sizefilter_contigs_re = qx($sizefilter_contigs_cmd); 
-$vwga->nline();
-$vplain->verbose($sizefilter_contigs_re) if $sizefilter_contigs_re;
-$vwga->exit('ERROR: Filtering contigs by size failed') if $?>> 8;
+if(exists $skip{8}){
+	$vwga->verbose('Skipping contig filter');
+}
+else{
+	$vwga->verbose('Filtering contigs by size');
+	$vwga->hline();
+	my $sizefilter_contigs_cmd = sizefilter_contigs_command();
+	$vbash->verbose( $sizefilter_contigs_cmd );
+	my $sizefilter_contigs_re = qx($sizefilter_contigs_cmd); 
+	$vwga->nline();
+	$vplain->verbose($sizefilter_contigs_re) if $sizefilter_contigs_re;
+	$vwga->exit('ERROR: Filtering contigs by size failed') if $?>> 8;
+}
 
 ########### Iteration
 
@@ -315,7 +373,7 @@ Returns the command to call jellyfish for counting.
 sub jellyfish_count_command{
 	my $cmd = "$opt_jellyfish_bin count -m $opt_jellyfish_kmer_size ";
 	$cmd .= "-o $opt_prefix"."_jf_parts ";
-	$cmd .= "-s 100000000 -t 20 --both-strands $opt_reads";
+	$cmd .= "-s 100000000 -t 20 --both-strands $opt_reads $opt_mates";
 	return $cmd;
 }
 
@@ -326,10 +384,15 @@ Returns the command to call jellyfish for merging.
 =cut
 
 sub jellyfish_merge_command{
-	my $cmd = "$opt_jellyfish_bin merge ";
-	$cmd .= "-o $opt_prefix"."_full.jf ";
-	$cmd .= "$opt_prefix"."_jf_parts*";
-	return $cmd;
+	if (-e "$opt_prefix"."_jf_parts_1"){
+		my $cmd = "$opt_jellyfish_bin merge ";
+		$cmd .= "-o $opt_prefix"."_full.jf ";
+		$cmd .= "$opt_prefix"."_jf_parts*";
+		return $cmd;
+	}
+	else{
+		return "ln -s $opt_prefix"."_jf_parts_0 $opt_prefix"."_full.jf"
+	}
 }
 
 =head2 jellyfish_histo_command
