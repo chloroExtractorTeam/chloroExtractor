50ffcc76b50d03131924944be4c8c406ada6ca3e
Frank Förster <frank.foerster@biozentrum.uni-wuerzburg.de>
2013-09-26 12:16:36 +0200

    Just a little try to allow interactive selection of peak range

---
+++ interactive.R
@@ -0,0 +1,38 @@
+
+
+num_points <- 10000
+xdat <- runif(num_points)*100000
+ydat <- rnorm(num_points)*1e7
+
+ready="p";
+xlimit<-range(xdat)
+ylimit<-range(ydat)
+peaklocation<-xlimit
+while (ready != "c")
+{
+    plot(xdat, ydat, xlim=xlimit, ylim=ylimit)
+    values<-locator(n=2, type="o")
+    cat("New xrange for plot (p) or final location (l) or cancel (c)?")
+    choice<-scan(n=1, what="character")
+    if (choice[1] == "p")
+        {
+            xlimit <- range(values$x)
+        }
+    if (choice[1] == "l")
+        {
+            peaklocation<-range(values$x)
+            dev.off()
+            break;
+        }
+    if (choice[1] == "c")
+        {
+            dev.off();
+            break;
+        }
+}
+
+peaklocation<-round(peaklocation);
+
+cat(paste("Final location of the peak is: ", peaklocation[1], "-", peaklocation[2], "\n", sep=""));
+
+    
