a83f2c0fc76cd12cb1db57fe7d6f4c7cb9642ebb
Frank Förster <frank.foerster@biozentrum.uni-wuerzburg.de>
2014-02-24 16:08:39 +0100

    New version of Markus script, but better readable with variables instead of hard coded filenames

--- bin/extend_contigs.sh
+++ bin/extend_contigs.sh
@@ -1,10 +1,39 @@
 #!/bin/bash
 
-grep ">" contigs_renamed.fa.contigs | perl -ne 's/>//;chomp; print "$_\t0\t200\n$_\t-200\t200\n"' >contigs_renamed.fa.contigs_end.coords
-SeqFilter --in contigs_renamed.fa.contigs --min-length 500 --out - | SeqFilter --substr contigs_renamed.fa.contigs_end.coords --substr-perl-style --out contigs_renamed.fa.contigs_end.fa
-/storage/software/amd64/bowtie2/bowtie2-build contigs_renamed.fa.contigs_end.fa contigs_renamed.fa.contigs_end
-/storage/software/amd64/bowtie2/bowtie2 --phred33 --al-conc contigs_renamed.fa.contigs_end_al --un-conc contigs_renamed.fa.contigs_end_un --no-unal -x contigs_renamed.fa.contigs_end -1 ../../cutoff_reads_out.fq -2 ../../cutoff_mates_out.fq -S contigs_renamed.fa.contigs_end.sam 
-grep -v "^@" contigs_renamed.fa.contigs_end.sam | cut -f1 >contigs_renamed.fa.contigs_end.al.ids
-SeqFilter --in ../../cutoff_reads_out.fq --ids contigs_renamed.fa.contigs_end.al.ids --out cutoff_reads_mapped.fq
-SeqFilter --in ../../cutoff_mates_out.fq --ids contigs_renamed.fa.contigs_end.al.ids --out cutoff_mates_mapped.fq
+INPUTFILE="contigs_renamed.fa.contigs"
+READS="../../cutoff_reads_out.fq"
+MATES="../../cutoff_mates_out.fq"
+
+OUT_READS="cutoff_reads_mapped.fq"
+OUT_MATES="cutoff_mates_mapped.fq"
+
+##
+## mapping parameter
+##
+
+MAPPINGREGION=200            # how long are the regions onto which will be mapped
+MINCONTIGLENGTH=500          # minimal contig length
+
+##
+## filenames used for temporary storage
+##
+
+COORDSFILE="$INPUTFILE"_end.coords
+CONTIGENDS="$INPUTFILE"_end.fa
+
+BOWTIE2_BASE="$INPUTFILE"_end
+BOWTIE2_ALIGNED="$BOWTIE2_BASE"_al
+BOWTIE2_UNALIGNED="$BOWTIE2_BASE"_un
+BOWTIE2_SAM="$BOWTIE2_BASE".sam
+
+MAPPED_READ_IDS="$INPUTFILE".al.ids
+
+grep ">"  "$INPUTFILE" | perl -ne 's/>//;chomp; print "$_\t0\t$MAPPINGREGION\n$_\t-$MAPPINGREGION\t$MAPPINGREGION\n"' > "$COORDSFILE"
+SeqFilter --in "$INPUTFILE" --min-length "$MINCONTIGLENGTH" --out - | SeqFilter --substr "$COORDSFILE" --substr-perl-style --out $CONTIG_ENDS
+/storage/software/amd64/bowtie2/bowtie2-build "$CONTIG_ENDS" "$BOWTIE2BASE"
+/storage/software/amd64/bowtie2/bowtie2 --phred33 --al-conc "$BOWTIE2_ALIGNED" --un-conc "BOWTIE2_UNALIGNED" --no-unal -x "$BOWTIE2_BASE" -1 "$READS" -2 "$MATES" -S "$BOWTIE2_SAM"
+grep -v "^@" "$BOWTIE2_SAM" | cut -f1 >"$MAPPED_READ_IDS"
+SeqFilter --in "$READS" --ids "$MAPPED_READ_IDS" --out "$OUT_READS"
+SeqFilter --in "$MATES" --ids "$MAPPED_READ_IDS" --out "$OUT_MATES"
+
 
