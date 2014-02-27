afc9bb4b8202bc724ab975298eac8a34d3326752
Frank Förster <frank.foerster@biozentrum.uni-wuerzburg.de>
2014-02-27 17:48:20 +0100

    Inital version of the assemble contigs script

---
+++ bin/assemble_contigs.pl
@@ -0,0 +1,190 @@
+#!/usr/bin/env perl
+
+package asc;
+
+=head1 NAME
+
+
+=head1 DESCRIPTION
+
+
+=head1 SYNOPSIS
+
+
+=head1 OPTIONS
+
+=over
+
+=item -c|--config
+
+Use user customized config file. Superseeds default config.
+
+=item --debug
+
+Verbose debug messages.
+
+=item -V|--version
+
+Display version.
+
+=item -h|--help
+
+Display this help.
+
+=item 
+
+=back
+
+=head1 CHANGELOG
+
+see git log.
+
+=head1 TODO
+
+=head1 CODE
+
+=cut
+
+#-----------------------------------------------------------------------------#
+# Modules
+
+# core
+use strict;
+use warnings;
+no warnings 'qw';
+
+use Carp;
+use Getopt::Long qw(:config no_ignore_case bundling);
+use Pod::Usage;
+use Log::Log4perl qw(:no_extra_logdie_message);
+use Log::Log4perl::Level;
+
+use Data::Dumper;
+$Data::Dumper::Sortkeys = 1;
+
+use FindBin qw($RealBin);
+use lib "$RealBin/../lib/";
+
+use File::Basename;
+use File::Copy;
+
+# additional modules
+use Cfg;
+use Cwd; # as we have to change directories
+use Fasta::Parser;
+
+
+#-----------------------------------------------------------------------------#
+# Globals
+
+our $VERSION = 0.01;
+
+# get a logger
+my $L = Log::Log4perl::get_logger();
+Log::Log4perl->init( \q(
+	log4perl.rootLogger                     = INFO, Screen
+	log4perl.appender.Screen                = Log::Log4perl::Appender::Screen
+	log4perl.appender.Screen.stderr         = 1
+	log4perl.appender.Screen.layout         = PatternLayout
+	log4perl.appender.Screen.layout.ConversionPattern = [%d{yy-MM-dd HH:mm:ss}] [%C] %m%n
+));
+
+
+#-----------------------------------------------------------------------------#
+# Config
+
+# core
+my $core_cfg = "$RealBin/../chloroExtractor.cfg";
+my %cfg = Cfg->Read_Cfg($core_cfg);
+
+# user defaults and overwrite core
+my $user_cfg;
+for(my $i=0; $i<@ARGV; $i++){
+        if($ARGV[$i] =~ /-c$|--config$/){
+                $user_cfg = $ARGV[$i+1];
+                last;
+        }
+}
+
+%cfg = (%cfg, Cfg->Read_Cfg($user_cfg)) if $user_cfg; # simple overwrite
+my %opt = %{$cfg{asr}};
+
+#TODO: custom config
+
+
+#-----------------------------------------------------------------------------#
+# GetOptions
+
+GetOptions( # use %opt (Cfg) as defaults
+	\%opt, qw(
+		version|V!
+		debug|D!
+		help|h!
+		config|c=s
+                out|o=s
+		workingdir|wd|w=s
+		in|i=s
+	)
+) or $L->logcroak('Failed to "GetOptions"');
+
+# help
+$opt{help} && pod2usage(1);
+
+# version
+if($opt{version}){
+	print "$VERSION\n"; 
+	exit 0;
+}
+
+# required stuff  
+for(qw(workingdir in out)){
+        pod2usage("required: --$_") unless defined ($opt{$_})
+};
+
+# debug level
+$L->level($DEBUG) if $opt{debug};
+$L->debug('Verbose level set to DEBUG');
+
+$L->debug(Dumper(\%opt));
+
+
+
+#-----------------------------------------------------------------------------#
+# MAIN
+
+$L->info('Assemble contigs');
+
+my $oldpwd;
+BEGIN{
+    $oldpwd = cwd();
+}
+## define rechange of working directory on exit
+END{
+    if (defined $oldpwd) { chdir($oldpwd) }
+}
+
+$L->debug("Changing current directory to $opt{workingdir}");
+chdir($opt{workingdir}) || $L->logdie("Unable to change to working directory $opt{workingdir}");
+
+#-----------------------------------------------------------------------------#
+
+=head1 AUTHOR
+
+Frank Foerster NAME S<frank.foerster@biozentrum.uni-wuerzburg.de>
+
+=cut
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
