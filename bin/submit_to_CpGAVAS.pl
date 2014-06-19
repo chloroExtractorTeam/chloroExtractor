38d1aa3ec4e70a9802589a2fb89dae04f7ece4df
Thomas Hackl <thomas.hackl@uni-wuerzburg.de>
2014-06-19 14:35:59 +0200

    submit_to_CpGAVAS using WWW::Mechanize::Firefox to solve Javascript Problem with field speciesName and field species dependency

--- bin/submit_to_CpGAVAS.pl
+++ bin/submit_to_CpGAVAS.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -w
 use strict;
-use WWW::Mechanize;
+use WWW::Mechanize::Firefox;
 use WWW::Mechanize::FormFiller;
 use URI::URL;
 
@@ -8,9 +8,8 @@ use URI::URL;
 my $usage = "USAGE perl submit_to_CpGAVAS.pl <chloro.fa> <species>";
 # Returns: <Species>\t<File>\t<Weblink to results>
 
-my $agent = WWW::Mechanize->new( autocheck => 1 );
+my $agent = WWW::Mechanize::Firefox->new( autocheck => 1 );
 my $formfiller = WWW::Mechanize::FormFiller->new();
-$agent->env_proxy();
 
 die "No species name given\n$usage\n" unless($ARGV[1]);
 
@@ -18,28 +17,28 @@ my ($species, $project, $file) = ($ARGV[1], "chloroExtractor", $ARGV[0]);
 
 die "File $file not found\n$usage\n" unless(-e $file);
 
-  $agent->get('http://www.herbalgenomics.org/0506/cpgavas/analyzer/annotate');
-  $agent->form_number(1) if $agent->forms and scalar @{$agent->forms};
-  $formfiller->add_filler( 'projectName' => Fixed => $project );
-  $formfiller->add_filler( 'kingdom' => Fixed => 'plants' );
-  $formfiller->add_filler( 'speciesName' => Fixed => $species );
-  $formfiller->add_filler( 'file1' => Fixed => $file );
-  $formfiller->add_filler( 'imageFormat' => Fixed => 'png' );
-  $formfiller->add_filler( 'emailAddress' => Fixed => '' );
-  $formfiller->add_filler( 'blastn_evalue' => Fixed => '1e-10' );
-  $formfiller->add_filler( 'blastx_evalue' => Fixed => '1e-10' );
-  $formfiller->add_filler( 'numOfHits' => Fixed => '10' );
-  $formfiller->add_filler( 'runRepeatMasker' => Fixed => 'no' );
-  $formfiller->add_filler( 'refdb_par1' => Fixed => '0' );
-  $formfiller->add_filler( 'species' => Fixed => 'all' );
-  $formfiller->add_filler( 'tRNAscan_par1' => Fixed => 'C' );
-  $formfiller->add_filler( 'tRNAscan_par2' => Fixed => 'O' );
-  $formfiller->add_filler( 'tRNAscan_par3' => Fixed => '15' );
-  $formfiller->add_filler( 'tRNAscan_par4' => Fixed => '116' );
-  $formfiller->add_filler( 'codontable_par1' => Fixed => '' );
-  $formfiller->add_filler( 'Clear' => Fixed => '' );
-  $formfiller->fill_form($agent->current_form);
-  $agent->submit();
+$agent->get('http://www.herbalgenomics.org/0506/cpgavas/analyzer/annotate');
+$agent->form_number(1) if $agent->forms and scalar @{$agent->forms};
+$agent->field('projectName' => $project);
+$agent->field('kingdom' => 'plants', [], ['change']);
+$agent->field('speciesName' => $species);
+$agent->field('file1' => $file);
+$agent->field('imageFormat' => 'png' );
+$agent->field('emailAddress' => '' );
+$agent->field('blastn_evalue' => '1e-10' );
+$agent->field('blastx_evalue' => '1e-10' );
+$agent->field('numOfHits' => '10' );
+$agent->field('runRepeatMasker' => 'no' );
+$agent->field('refdb_par1' => '0' );
+$agent->field('tRNAscan_par1' => 'C' );
+$agent->field('tRNAscan_par2' => 'O' );
+$agent->field('tRNAscan_par3' => '15' );
+$agent->field('tRNAscan_par4' => '116' );
+$agent->field('codontable_par1' => '');
+$agent->field('species' => 'all' );
+
+#sleep 10000;
+$agent->click_button(value => 'Submit');
 
 print "$species\t$file\t",$agent->base(),"\n";
 
