29d45e3dced79386a90981fc3dde90f13c0af434
Thomas Hackl <thomas.hackl@uni-wuerzburg.de>
2014-06-19 18:57:39 +0200

    [rCG] Accept submission id list as parameter

--- bin/retrieve_from_CpGAVAS.sh
+++ bin/retrieve_from_CpGAVAS.sh
@@ -1,4 +1,4 @@
-for ID in $(cut -f2 -d"=" ../project_ids.list)
+for ID in $(cut -f2 -d"=" $1)
 do
 	wget http://www.herbalgenomics.org/0506/cpgavas/docroot/tmp/dir_${ID}/${ID}.htm -r -l 1 -nH --cut-dir 5 -P $ID
 done
