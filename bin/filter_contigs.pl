dd896e6ed60aabd530c201f646ed136e5437372b
Thomas Hackl <thomas.hackl@uni-wuerzburg.de>
2014-07-23 11:06:50 +0200

    + [fic] upgraded opt-cfg prio handling

--- bin/filter_contigs.pl
+++ bin/filter_contigs.pl
@@ -70,7 +70,7 @@ use warnings;
 no warnings 'qw';
 
 use Carp;
-use Getopt::Long qw(:config no_ignore_case bundling);
+use Getopt::Long qw(:config no_ignore_case);
 use Pod::Usage;
 use Log::Log4perl qw(:no_extra_logdie_message);
 use Log::Log4perl::Level;
@@ -78,7 +78,7 @@ use Log::Log4perl::Level;
 use Data::Dumper;
 $Data::Dumper::Sortkeys = 1;
 
-use FindBin qw($RealBin);
+use FindBin qw($RealBin $Script);
 use lib "$RealBin/../lib/";
 
 use File::Basename;
@@ -91,43 +91,34 @@ use Cfg;
 #-----------------------------------------------------------------------------#
 # Globals
 
-our $VERSION = 0.01;
+our $VERSION = 0.02;
+
+our $ID = 'fic';
 
 # get a logger
 my $L = Log::Log4perl::get_logger();
-Log::Log4perl->init( \q(
-	log4perl.rootLogger                     = INFO, Screen
-	log4perl.appender.Screen                = Log::Log4perl::Appender::Screen
-	log4perl.appender.Screen.stderr         = 1
-	log4perl.appender.Screen.layout         = PatternLayout
-	log4perl.appender.Screen.layout.ConversionPattern = [%d{yy-MM-dd HH:mm:ss}] [fic] %m%n
-));
 
+my $log_cfg = 'log4perl.rootLogger                     = INFO, Screen
+log4perl.appender.Screen                = Log::Log4perl::Appender::Screen
+log4perl.appender.Screen.stderr         = 1
+log4perl.appender.Screen.layout         = PatternLayout
+log4perl.appender.Screen.layout.ConversionPattern = [%d{yy-MM-dd HH:mm:ss}] ['.$ID.'] %m%n
+';
 
-#-----------------------------------------------------------------------------#
-# Config
-
-# core
-my $core_cfg = "$RealBin/../chloroExtractor.cfg";
-my %cfg = Cfg->Read_Cfg($core_cfg); 
-
-# user defaults and overwrite core
-my $user_cfg;
-for(my $i=0; $i<@ARGV; $i++){
-        if($ARGV[$i] =~ /-c$|--config$/){
-                $user_cfg = $ARGV[$i+1];
-                last;
-        }
-}
-
-%cfg = (%cfg, Cfg->Read_Cfg($user_cfg)) if $user_cfg; # simple overwrite
-
-my %opt = %{$cfg{fic}};
-
+Log::Log4perl->init( \$log_cfg );
 
 
 #-----------------------------------------------------------------------------#
 # GetOptions
+
+# opt: multi-params need to be initiated with ARRAYREF!
+my %opt = (
+    config => [],
+    );
+
+# Setup defaults
+my %def = (
+    );
 
 GetOptions( # use %opt (Cfg) as defaults
 	\%opt, qw(
@@ -140,7 +131,7 @@ GetOptions( # use %opt (Cfg) as defaults
 		version|V!
 		debug|D!
 		help|h!
-		config|c=s
+		config|c=s{,}
 	)
 ) or $L->logcroak('Failed to "GetOptions"');
 
@@ -153,18 +144,60 @@ if($opt{version}){
 	exit 0;
 }
 
+##----------------------------------------------------------------------------##
+# Config
 
-# required stuff  
-for(qw(in blast_db blast_binary)){
-        pod2usage("required: --$_") unless defined ($opt{$_})
-};
+my %cfg;
+
+# core
+my $core_cfg = "$RealBin/../".basename($Script, qw(.pl)).".cfg";
+
+if(-e $core_cfg){
+    $opt{core_config} = File::Spec->rel2abs($core_cfg);
+    %cfg = (%cfg, Cfg->Read($opt{core_config}, $ID));
+}
+
+
+
+# read all configs
+if (@{$opt{config}}){
+    foreach my $cfg ( @{$opt{config}} ){
+	# $L->info("Reading config $cfg");
+	$cfg=File::Spec->rel2abs($cfg);
+	%cfg = (%cfg, Cfg->Read($cfg, $ID));
+    }
+}
+
+# create template for user cfg
+if(defined $opt{create_config}){
+    pod2usage(-msg => 'To many arguments', -exitval=>1) if @ARGV > 1;
+    my $user_cfg = Cfg->Copy($core_cfg, $opt{create_config}) or $L->logdie("Creatring config failed: $!");
+    $L->info("Created config file: $user_cfg");
+    exit 0;
+}
+
+
+# Merge opt and cfg
+%opt = (%cfg, %opt);
 
 
 # debug level
-$L->level($DEBUG) if $opt{debug};
-$L->debug('Verbose level set to DEBUG');
+$opt{quiet} && $L->level($WARN);
+$opt{debug} && $L->level($DEBUG);
 
 $L->debug(Dumper(\%opt));
+
+
+
+##----------------------------------------------------------------------------##
+# required	
+for(qw(in blast_db blast_binary)){
+    if(ref $opt{$_} eq 'ARRAY'){
+	pod2usage("required: --$_") unless @{$opt{$_}}
+    }else{
+	pod2usage("required: --$_") unless defined ($opt{$_})
+    }
+};
 
 
 
@@ -173,7 +206,7 @@ $L->debug(Dumper(\%opt));
 
 $opt{out} ||= basename($opt{in}, qw(.fasta .fas .fa .FASTA .FAS .FA)).".fil.fa";
 
-my @blast_cmd  = ($opt{blast_binary},'-db', $opt{blast_db}, '-query', $opt{in}, @{$opt{blast_options}});
+my @blast_cmd  = ($opt{blast_binary} ? $opt{blast_binary}."/blastn" : "blastn",'-db', $opt{blast_db}, '-query', $opt{in}, @{$opt{blast_options}});
 $L->debug("Running: @blast_cmd");
 my @ids = qx(@blast_cmd);
 
