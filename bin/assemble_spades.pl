b375396eb9682e1164d8247cda2af3a8e8e00d4b
Thomas Hackl <thomas.hackl@uni-wuerzburg.de>
2014-09-16 09:46:56 +0200

    + [assemble_spades.pl] Added wrapper script for SPAdes call, basically just a path-through, primarily handles SPAdes annoying logs

---
+++ bin/assemble_spades.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/env perl
+
+use warnings;
+use strict;
+
+my $i;
+for($i=0; $i<@ARGV; $i++){
+    last if $ARGV[$i] eq "-o";
+}
+my $pre = $ARGV[$i+1];
+open(LOG, ">", $pre.".log") or die "$!: ",$pre,".log\n";
+
+open(SPADES , "spades.py @ARGV |") or die "$!: Running SPAdes\n"; 
+print LOG $_ while <SPADES>;
+close SPADES;
+close LOG;
+
+qx(ln -s $pre/scaffolds.fasta $pre.fa);
