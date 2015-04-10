f45193abbebd0ef8e4c6a6c77d0534d33595ce76
Frank Förster <frank.foerster@biozentrum.uni-wuerzburg.de>
2015-04-10 15:17:18 +0200

    assembe_spades.pl adding a fixed option to skip the correcton step in spades assembly pipeline

--- bin/assemble_spades.pl
+++ bin/assemble_spades.pl
@@ -10,7 +10,7 @@ for($i=0; $i<@ARGV; $i++){
 my $pre = $ARGV[$i+1];
 open(LOG, ">", $pre.".log") or die "$!: ",$pre,".log\n";
 
-open(SPADES , "spades.py @ARGV |") or die "$!: Running SPAdes\n"; 
+open(SPADES , "spades.py --only-assembler @ARGV |") or die "$!: Running SPAdes\n";
 print LOG $_ while <SPADES>;
 close SPADES;
 close LOG;
