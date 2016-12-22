46944a181052dcf0d6e50dbe1b46398004abb647
PfaffS <simon.pfaff@stud-mail.uni-wuerzburg.de>
2016-12-22 10:51:50 +0100

    new File added: find_cyclic_graph.pl

---
+++ bin/find_cyclic_graph.pl
@@ -0,0 +1,178 @@
+#!/usr/bin/env perl
+
+=head1 NAME
+
+find_cyclic_graph.pl
+
+=head1 SYNOPSIS
+
+    $ perl find_cyclic_graph.pl -i graph.fastg
+
+=head1 OPTIONS
+
+=over
+
+=item -i|--in <FASTG FILE>
+    
+    Fastg Graph File
+
+=item -o|--out <NAME> [basename "--in".fa]
+
+    Output Filename
+
+=item -V|--version
+
+Display version.
+
+=item -h|--help
+
+Display this help.
+
+=back
+
+=cut
+#----------------------#
+# Modules
+
+use strict;
+use warnings;
+
+use Getopt::Long qw(:config no_ignore_case);
+use Pod::Usage;
+use Log::Log4perl qw(:no_extra_logdie_message);
+use Log::Log4perl::Level;
+
+
+use Data::Dumper;
+
+use FindBin qw($RealBin $Script);
+use lib "$RealBin/../lib/";
+
+#--------------------#
+# Globals
+
+our $Version = 0.01;
+
+our $ID = 'fcg';
+
+# Log4perl logger
+
+my $L = Log::Log4perl::get_logger();
+
+my $log_cfg = 'log4perl.rootLogger                     = INFO, Screen
+log4perl.appender.Screen                = Log::Log4perl::Appender::Screen
+log4perl.appender.Screen.stderr         = 1
+log4perl.appender.Screen.layout         = PatternLayout
+log4perl.appender.Screen.layout.ConversionPattern = [%d{yy-MM-dd HH:mm:ss}] ['.$ID.'] %m%n
+';
+
+#------------------#
+# Get Options
+
+# opt: multi-params need to be initiated with ARRAYREF!
+my %opt = (
+    config => [],
+    );
+
+# Setup defaults
+my %def = (
+    );
+
+GetOptions( # use %opt (Cfg) as defaults
+	\%opt, qw(
+                in|i=s
+                out|o=s
+               	version|V!
+	       	help|h!
+                debug|D!
+			)
+
+) or $L->logcroak('Failed to "GetOptions"');
+
+
+# help
+$opt{help} && pod2usage(1);
+
+# version
+if($opt{version}){
+	print "$VERSION\n"; 
+	exit 0;
+}
+
+#--------------------------#
+# Config
+
+my %cfg;
+
+#core 
+
+if(-e $core_cfg){
+    $opt{core_config} = File::Spec->rel2abs($core_cfg);
+    %cfg = (%cfg, Cfg->Read($opt{core_config}, $ID));
+}
+
+
+
+# read all configs
+if (@{$opt{config}}){
+    foreach my $cfg ( @{$opt{config}} ){
+	# $L->info("Reading config $cfg");
+	$cfg=File::Spec->rel2abs($cfg);
+	%cfg = (%cfg, Cfg->Read($cfg, $ID));
+    }
+}
+
+# create template for user cfg
+if(defined $opt{create_config}){
+    pod2usage(-msg => 'To many arguments', -exitval=>1) if @ARGV > 1;
+    my $user_cfg = Cfg->Copy($core_cfg, $opt{create_config}) or $L->logdie("Creatring config failed: $!");
+    $L->info("Created config file: $user_cfg");
+    exit 0;
+}
+
+
+# Merge opt and cfg
+%opt = (%cfg, %opt);
+
+
+# debug level
+$opt{quiet} && $L->level($WARN);
+$opt{debug} && $L->level($DEBUG);
+
+$L->debug(Dumper(\%opt));
+
+#---------------------------#
+# required
+
+
+
+
+
+
+#------------------------#
+# MAIN
+
+$opt{out} ||= basename($opt{in}, qw(.fastg).".fa";
+
+# Check infile Format
+
+
+
+
+
+
+
+## DO YOUR THING GRAPH!
+
+
+#--------------------------#
+# Methodes
+
+
+=head1 AUTHOR
+
+Frank Förster S<frank.foerster@uni-wuerzburg.de>
+Simon Pfaff S<simon.pfaff@stud-mail.uni-wuerzburg.de>
+Aaron Sigmund S<aaron.sigmund@stud-mail.uni-wuerzburg.de>
+
+=cut
